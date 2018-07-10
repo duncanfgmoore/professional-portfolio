355d0e4407f3775a0c7f8b02133bad7f26d432b0
Duncan <duncan@Duncans-MacBook-Pro.local>
2018-07-10 14:55:13 -0500

    fade working

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
 
-     
-   
+    $("#myName").fadeTo(1000, 1);
+    $("#belowName").fadeTo(1000, 1); 
+    $("#profileImage").fadeTo(1000, 1);
+    $("#personalLinks").fadeTo(1000, 1);
 
 
 
